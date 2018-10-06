0bb4539504693b64dd5065e720beb4b88a00e2cf
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-05 23:34:31 -0500

    Ex: answer to steps 4 equals staircase(3-1) + staircase(3-2) + ...staircase(3-m)

--- steps.rb
+++ steps.rb
@@ -7,17 +7,26 @@ p "Staircase 2!"
 # Return number of ways to reach top of staircase
 def staircase_2 steps, m, array=[]
 	# If staircase has 0-1 steps, only one way to reach top
-	if (steps <= 1)
+	if steps <= 1
 		return 1
 	end
 	
 	# If we already found answer from before, its in array.
-	if (array[steps])
+	if array[steps]
 		return array[steps]
 	end
 
 	# Add up results in this variable.
-	results = 0
+	result = 0
+
+	# Ex: answer to steps 4 equals staircase(3-1) + staircase(3-2) + ...staircase(3-m)
+	for i in 1..m
+		# Only if steps >= 0
+		if steps - i >= 0
+			result += staircase_2 steps - i, m, array
+		end
+	end
+				
 	
 
 end	

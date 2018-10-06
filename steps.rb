ec54fcb5f9bb9d1052b7fd79ee3a83c77b6861b9
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-05 23:22:30 -0500

    If staircase has 0-1 steps, only one way to reach top

--- steps.rb
+++ steps.rb
@@ -4,5 +4,10 @@ p "Staircase 2!"
 # steps: amount of steps on staircase
 # m: maximum amount of steps that can be taken at once
 # array: We will use dynamic programming to store results here.
+# Return number of ways to reach top of staircase
 def staircase_2 steps, m, array=[]
+	# If staircase has 0-1 steps, only one way to reach top
+	if (steps <= 1)
+		return 1
+	end	
 end	

891e1e04a3c641715173aaab7ed6c36cee13a62f
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-05 23:20:09 -0500

    Defined function

--- steps.rb
+++ steps.rb
@@ -1,1 +1,8 @@
 p "Staircase 2!"
+
+# Function takes in 3 params
+# steps: amount of steps on staircase
+# m: maximum amount of steps that can be taken at once
+# array: We will use dynamic programming to store results here.
+def staircase_2 steps, m, array=[]
+end	
